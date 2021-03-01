102536a4565cc48baf24d51c499ac8177aa37373
Omid.N <nejadabbasio@gmail.com>
2021-03-01 04:30:48 +0330

    Initial commit

---
+++ src/main/java/net/omidn/pdfcreator/Main.java
@@ -0,0 +1,21 @@
+package net.omidn.pdfcreator;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/net/omidn/pdfcreator/ui/MainFrame.java
@@ -0,0 +1,4 @@
+package net.omidn.pdfcreator.ui;
+
+public class MainFrame {
+}
