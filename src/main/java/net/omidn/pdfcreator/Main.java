586067b7d32247e512a86944b2cf633d09d9bcac
Omid.N <nejadabbasio@gmail.com>
2021-03-02 09:38:04 +0330

    Added menu items to menu bar, optimized imports

--- src/main/java/net/omidn/pdfcreator/Main.java
+++ src/main/java/net/omidn/pdfcreator/Main.java
@@ -1,11 +1,10 @@
 package net.omidn.pdfcreator;
 
 import javafx.application.Application;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -14,7 +13,19 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         VBox rootContainer = new VBox();
-        MenuBar appMenuBar = new MenuBar(new Menu("File"), new Menu("123"));
+        // menu bar
+        Menu fileMenu = new Menu("File");
+        MenuItem addFilesMenuItem = new MenuItem("Add Files...");
+        MenuItem saveAsMenuItem = new MenuItem("Save As...");
+        MenuItem quiteMenuItem = new MenuItem("Quite");
+        fileMenu.getItems().addAll(addFilesMenuItem, saveAsMenuItem, quiteMenuItem);
+
+        // help menu
+        Menu helpMenu = new Menu("Help");
+        MenuItem aboutMenuItem = new MenuItem("About the App");
+        helpMenu.getItems().add(aboutMenuItem);
+
+        MenuBar appMenuBar = new MenuBar(fileMenu, helpMenu);
 
         rootContainer.getChildren().add(appMenuBar);
         Scene scene = new Scene(rootContainer, 700, 400);
