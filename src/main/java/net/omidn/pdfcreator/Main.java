933e90036732db92592219297fe210accdd02c56
Omid.N <nejadabbasio@gmail.com>
2021-03-10 01:35:22 +0330

    Turned all view cotrols into fields :(

--- src/main/java/net/omidn/pdfcreator/Main.java
+++ src/main/java/net/omidn/pdfcreator/Main.java
@@ -11,46 +11,65 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    // view controls
+    Menu fileMenu;
+    VBox rootContainer;
+    MenuItem addFilesMenuItem;
+    MenuItem saveAsMenuItem;
+    MenuItem quiteMenuItem;
+    Menu helpMenu;
+    MenuItem aboutMenuItem;
+    MenuBar appMenuBar;
+    HBox mainAreaHBox;
+
+    ListView<String> fileListView;
+
+    VBox btnContainer;
+    Button removeBtn;
+    Button moveUpBtn;
+    Button moveDownBtn;
+    Scene scene;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        VBox rootContainer = new VBox();
+    public void start(Stage primaryStage) throws Exception {
+        rootContainer = new VBox();
         // menu bar
-        Menu fileMenu = new Menu("File");
-        MenuItem addFilesMenuItem = new MenuItem("Add Files...");
-        MenuItem saveAsMenuItem = new MenuItem("Save As...");
-        MenuItem quiteMenuItem = new MenuItem("Quite");
+        fileMenu = new Menu("File");
+        addFilesMenuItem = new MenuItem("Add Files...");
+        saveAsMenuItem = new MenuItem("Save As...");
+        quiteMenuItem = new MenuItem("Quite");
         fileMenu.getItems().addAll(addFilesMenuItem, saveAsMenuItem, quiteMenuItem);
 
         // help menu
-        Menu helpMenu = new Menu("Help");
-        MenuItem aboutMenuItem = new MenuItem("About the App");
+        helpMenu = new Menu("Help");
+        aboutMenuItem = new MenuItem("About the App");
         helpMenu.getItems().add(aboutMenuItem);
 
-        MenuBar appMenuBar = new MenuBar(fileMenu, helpMenu);
+        appMenuBar = new MenuBar(fileMenu, helpMenu);
 
 
         // mainArea
-        HBox mainAreaHBox = new HBox();
+        mainAreaHBox = new HBox();
         VBox.setVgrow(mainAreaHBox, Priority.ALWAYS);
 
         //ListView
-        ListView<String> fileListView = new ListView<>();
+        fileListView = new ListView<>();
         fileListView.getItems().add("Omid");
         fileListView.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 
         HBox.setHgrow(fileListView, Priority.ALWAYS);
         // adding list editing buttons
-        VBox btnContainer = new VBox();
-        Button removeBtn = new Button("Remove");
-        Button moveUpBtn = new Button("Move Up");
-        Button moveDownBtn = new Button("Move Down");
+        btnContainer = new VBox();
+        removeBtn = new Button("Remove");
+        moveUpBtn = new Button("Move Up");
+        moveDownBtn = new Button("Move Down");
         btnContainer.getChildren().addAll(removeBtn, moveUpBtn, moveDownBtn);
 
         removeBtn.setMaxWidth(Double.MAX_VALUE);
         moveDownBtn.setMaxWidth(Double.MAX_VALUE);
         moveUpBtn.setMaxWidth(Double.MAX_VALUE);
         // add margins to buttons
-        btnContainer.setPadding( new Insets(15));
+        btnContainer.setPadding(new Insets(15));
 
         // TODO turn local variables into fields :(
 
@@ -58,7 +77,7 @@ public class Main extends Application {
         mainAreaHBox.getChildren().add(btnContainer);
 
         rootContainer.getChildren().addAll(appMenuBar, mainAreaHBox);
-        Scene scene = new Scene(rootContainer, 700, 400);
+        scene = new Scene(rootContainer, 700, 400);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Prototype");
         primaryStage.show();
