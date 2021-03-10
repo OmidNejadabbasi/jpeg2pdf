69cbe98205d58c66bcab7cf84e171ac6c3c603f5
Omid.N <nejadabbasio@gmail.com>
2021-03-10 22:19:37 +0330

    Added PDFbox dependency and created converter file

---
+++ src/main/java/net/omidn/pdfcreator/JPEG2PDFCore.java
@@ -0,0 +1,12 @@
+package net.omidn.pdfcreator;
+
+import java.io.File;
+import java.util.List;
+
+public class JPEG2PDFCore {
+
+    public static void saveFileAsPDF(List<String> files, File destFile){
+        // TODO implement :)
+    }
+
+}
